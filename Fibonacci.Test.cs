a44af6970f6d277b04f1196a4f602df502f39938
KotsmanV <vassilis.kotsman@gmail.com>
2021-04-01 19:13:57 +0300

    Initial commit

---
+++ Fibonacci.Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using FibonacciNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNunit.Test
+{
+    [TestFixture]
+    class Fibonacci
+    {
+        [TestCase(2,1)]
+        [TestCase(3,2)]
+        [TestCase(5,3)]
+        [TestCase(8,4)]
+        public void TestFibonacci(double expected, double input)
+        {
+            Assert.AreEqual(expected, FiboNumbers.FiboNext(input));
+        }
+
+
+    }
+}
